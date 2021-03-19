0ec0a47e6117ca98f234d3892cf1259a310a6cd2
DESKTOP-HPASRTK\Jonathan Bersabal <Jonathan>
2021-03-19 14:38:59 +0800

    nag ewdit aku sa index

--- index.php
+++ index.php
@@ -1,10 +1,11 @@
 <html>
 <head>
-    <title>backendActivity</title>
+    <title>Githubtest</title>
     <?php include("includes/links/links.php"); ?>
 </head>
 
 <body>
+<h1>JOnbers pOgi</h1>
 <?php include("components/navigation/navbar.php"); ?>
 <?php include("views/indexviews.php"); ?>
 <?php include("includes/scripts/scripts.php"); ?>
